6d35b7e92b66f79f9e43ef422cd4a254d155d648
thmoreiracosta <thiagomoreiracosta@gmail.com>
2021-03-14 15:28:48 -0300

    Add project files.

---
+++ exercicios/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInit, horaFinal, duracao;
+            duracao = 0;           
+
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInit = int.Parse(vet[0]);
+            horaFinal = int.Parse(vet[1]);
+
+            if (horaFinal > horaInit)
+            {
+                for (int f = horaInit + 1; f <= horaFinal; f++)
+                {
+                    duracao += 1;
+                }
+            } 
+            else
+            {
+                for (int i = horaInit + 1; i <= horaFinal + 24; i++)
+                {
+                    duracao += 1;
+                }
+            }            
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
